5326b88604b12be1717fd06da702a0a3aa5bb2e8
MERT EFE ŞENSOY <mefe.sensoy@tedu.edu.tr>
2026-01-17 20:48:34 +0300

    fix(test): Fix Jest configuration conflicts and add missing babel-preset-expo

--- expo/jest.config.js
+++ expo/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     preset: 'jest-expo',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
     collectCoverageFrom: [
